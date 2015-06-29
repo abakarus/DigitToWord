b9817f96380ec0b6fa69d7659b3acdb287a68763
abakarus <abakarus@gmail.com>
2015-06-29 15:02:36 +0500

    20150629

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace DigitToWord
     {
         static void Main(string[] args)
         {
-            int reqem = 5542;
+            int reqem = 20542;
             string netice = "";
 
             string[] strteklik = {"Bir ", "İki ", "Üç ", "Dörd ", "Beş ", "Altı ", "Yeddi ", "Səkkiz ", "Doqquz "};
@@ -27,15 +27,23 @@ namespace DigitToWord
             int ikinci = int.Parse(strreqem.Substring(strreqem.Length-2, 1));
 
 
-            teklik = strteklik[int.Parse(strreqem.Substring(strreqem.Length - 1, 1))-1];
-            onluq = stronluq[int.Parse(strreqem.Substring(strreqem.Length - 2, 1))-1];
-            yuzluk = strteklik[int.Parse(strreqem.Substring(strreqem.Length - 3, 1)) - 1] + stryuzluk[0]; 
-
-
-            string result = yuzmilyon + onmilyon + tekmilyon + yuzminlik + onminlik + tekminlik + yuzluk + onluq + teklik;
+            teklik = int.Parse(strreqem.Substring(strreqem.Length - 1, 1)) >0 ? strteklik[int.Parse(strreqem.Substring(strreqem.Length - 1, 1))-1] : "";
+            onluq =  int.Parse(strreqem.Substring(strreqem.Length - 2, 1)) >0  ? stronluq[int.Parse(strreqem.Substring(strreqem.Length - 2, 1))-1] : "";
+            yuzluk = int.Parse(strreqem.Substring(strreqem.Length - 3, 1)) >0  ? strteklik[int.Parse(strreqem.Substring(strreqem.Length - 3, 1)) - 1] + "Yüz " : "";
 
 
 
+            tekminlik = int.Parse(strreqem.Substring(strreqem.Length - 4, 1)) > 0 ? strteklik[int.Parse(strreqem.Substring(strreqem.Length - 4, 1)) - 1] : "";
+            onminlik = int.Parse(strreqem.Substring(strreqem.Length - 5, 1)) > 0 ? stronluq[int.Parse(strreqem.Substring(strreqem.Length - 5, 1)) - 1] : "";
+            yuzminlik = int.Parse(strreqem.Substring(strreqem.Length - 6, 1)) > 0 ? strteklik[int.Parse(strreqem.Substring(strreqem.Length - 6, 1)) - 1] + "Yüz " : "";
+            string min = int.Parse(strreqem.Substring(0, 3)) > 0 ? "Min " : ""; 
+
+            
+            
+            string result = yuzminlik + onminlik + tekminlik + min + yuzluk + onluq + teklik;
+
+
+            Console.WriteLine(result);
         }
     }
 }
